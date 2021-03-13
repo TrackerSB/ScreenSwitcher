a5c27c4cb2fc55cb76165b6eb2dde62852b786a5
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-03-13 06:56:48 +0100

    Introduce screen overlay

--- src/main/java/bayern/steinbrecher/screenswitcher/ScreenManager.java
+++ src/main/java/bayern/steinbrecher/screenswitcher/ScreenManager.java
@@ -1,15 +1,19 @@
 package bayern.steinbrecher.screenswitcher;
 
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
@@ -20,10 +24,11 @@ import java.util.logging.Logger;
 public class ScreenManager {
     private static final Logger LOGGER = Logger.getLogger(ScreenManager.class.getName());
     private final StackPane screenStack = new StackPane();
+    private final StackPane mainStack = new StackPane(screenStack);
     private final Map<Node, RunnableFuture<?>> waitForWizardEndedScreens = new HashMap<>();
 
     public ScreenManager(@NotNull Stage screenBaseStage) {
-        screenBaseStage.setScene(new Scene(screenStack));
+        screenBaseStage.setScene(new Scene(mainStack));
     }
 
     /**
@@ -89,6 +94,55 @@ public class ScreenManager {
             if (callbackTask != null) {
                 callbackTask.run();
             }
+        }
+    }
+
+    /**
+     * @since 0.2.1
+     */
+    public void showOverlay(@NotNull String message) {
+        if (isOverlayActive()) {
+            final Optional<Text> overlayMessage = mainStack.getChildren()
+                    .stream()
+                    .filter(c -> c instanceof Text)
+                    .map(c -> (Text) c)
+                    .findFirst();
+            if (overlayMessage.isPresent()) {
+                overlayMessage.get()
+                        .setText(message);
+            } else {
+                LOGGER.log(Level.WARNING, "Could not change overlay text");
+            }
+        } else {
+            Rectangle overlayBackground = new Rectangle();
+            overlayBackground.widthProperty()
+                    .bind(mainStack.widthProperty());
+            overlayBackground.heightProperty()
+                    .bind(mainStack.heightProperty());
+            overlayBackground.getStyleClass()
+                    .add("screen-overlay");
+
+            Text overlayMessage = new Text(message);
+
+            mainStack.getChildren()
+                    .addAll(overlayBackground, overlayMessage);
+        }
+    }
+
+    /**
+     * @since 0.2.1
+     */
+    public boolean isOverlayActive() {
+        return mainStack.getChildren().size() > 1;
+    }
+
+    /**
+     * @since 0.2.1
+     */
+    public void hideOverlay() {
+        if (isOverlayActive()) {
+            ObservableList<Node> children = mainStack.getChildren();
+            children.remove(1, children.size());
         }
     }
 }
