b63a55f5bc2c76880c9d9bf0f471bd5f2af2271d
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-08-10 13:20:59 +0200

    Gradle: Update packages

--- src/main/java/bayern/steinbrecher/screenswitcher/WizardScreen.java
+++ src/main/java/bayern/steinbrecher/screenswitcher/WizardScreen.java
@@ -1,6 +1,5 @@
 package bayern.steinbrecher.screenswitcher;
 
-import bayern.steinbrecher.wizard.EmbeddedWizardPage;
 import bayern.steinbrecher.wizard.Wizard;
 import bayern.steinbrecher.wizard.WizardPage;
 import bayern.steinbrecher.wizard.WizardState;
@@ -11,36 +10,16 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public abstract class WizardScreen<R> extends Screen<WizardScreenController> {
-    private static final Logger LOGGER = Logger.getLogger(WizardScreen.class.getName());
     private final Wizard wizard;
 
     protected WizardScreen() throws LoadException {
         super(WizardScreen.class.getResource("WizardScreen.fxml"),
                 ResourceBundle.getBundle("bayern.steinbrecher.screens.WizardScreen"));
-        Map<String, EmbeddedWizardPage<?>> embeddedWizardPages = generatePages()
-                .entrySet()
-                .stream()
-                .map(entry -> {
-                    try {
-                        return Map.entry(entry.getKey(), entry.getValue().generateEmbeddableWizardPage());
-                    } catch (LoadException ex) {
-                        LOGGER.log(Level.SEVERE,
-                                String.format("Could not generate wizard page for '%s'", entry.getKey()), ex);
-                        return null;
-                    }
-                })
-                .filter(Objects::nonNull)
-                .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
-        this.wizard = Wizard.create(embeddedWizardPages);
+        this.wizard = Wizard.create(generatePages());
     }
 
     @Override
